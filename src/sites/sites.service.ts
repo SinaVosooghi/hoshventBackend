3bc7cb8081ef71d9b52b2ea60b8dacca8c6de6c3
Seena <sinahamzezade@gmail.com>
2023-08-19 23:06:29 +0330

    Update sites.service.ts

--- src/sites/sites.service.ts
+++ src/sites/sites.service.ts
@@ -11,7 +11,7 @@ import { Like, Repository } from 'typeorm';
 import { GetSitesArgs } from './dto/get-items';
 import { UsersService } from 'src/users/users.service';
 import { imageUploader } from 'src/utils/imageUploader';
-import { writeFile, copyFile } from 'fs';
+import { writeFile, copyFile, cp } from 'fs';
 import { exec } from 'child_process';
 
 @Injectable()
@@ -63,7 +63,7 @@ export class SitesService {
     const src = `/var/www/tenant`;
     const dist = `/var/www/${item.domain}`;
 
-    copyFile(src, dist, () => {
+    cp(src, dist, () => {
       console.log('Files copied');
     });
 

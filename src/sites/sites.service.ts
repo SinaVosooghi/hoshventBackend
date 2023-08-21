15e80b7eaaac225d8b4c6933141d49ab93a21306
Seena <sinahamzezade@gmail.com>
2023-08-21 16:41:02 +0330

    Update sites.service.ts

--- src/sites/sites.service.ts
+++ src/sites/sites.service.ts
@@ -51,7 +51,7 @@ export class SitesService {
         index index.html index.htm index.php;
     
         location / {
-            proxy_pass http://146.19.212.55:${item.port};
+            proxy_pass http://localhost:${item.port};
             proxy_http_version 1.1;
             proxy_set_header Upgrade $http_upgrade;
             proxy_set_header Connection 'upgrade';
@@ -97,19 +97,93 @@ export class SitesService {
         NEXT_PUBLIC_JITSI_APP_ID=""
         NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY=""
         NEXT_PUBLIC_SITE=${item.id}
-        
       `,
         async (err) => {
           if (err) {
             console.error(err);
           }
           console.log('ENV Created');
-          await exec(
-            `cd ${dist} && pm2 start yarn --name "${item.slug}" bash -- start`,
-          );
 
-          await exec(
-            `sudo ln -s /etc/nginx/sites-available/${item.domain}.conf /etc/nginx/sites-enabled/`,
+          await writeFile(
+            `/var/www/${item.domain}/package.json`,
+            `{
+              "name": "event-fronts",
+              "version": "0.1.0",
+              "private": true,
+              "scripts": {
+                "dev": "next dev -p ${item.port}",
+                "build": "next build",
+                "start": "next start -p ${item.port}",
+                "lint": "next lint"
+              },
+              "dependencies": {
+                "@ant-design/icons": "^5.0.1",
+                "@apollo/client": "^3.7.4",
+                "@devmehq/react-qr-code": "^1.0.9",
+                "@fullcalendar/core": "^6.1.8",
+                "@fullcalendar/daygrid": "^6.1.8",
+                "@fullcalendar/react": "^6.1.8",
+                "@jitsi/react-sdk": "^1.3.0",
+                "@next/bundle-analyzer": "^13.4.7",
+                "@next/font": "^13.1.2",
+                "@rematch/core": "^2.2.0",
+                "@stripe/react-stripe-js": "^2.1.0",
+                "@stripe/stripe-js": "^1.52.1",
+                "antd": "^5.6.0",
+                "axios": "^1.2.2",
+                "chop-lines": "^1.15.7",
+                "cookies-next": "^2.1.1",
+                "graphql": "^16.6.0",
+                "html-react-parser": "^3.0.12",
+                "insane": "^2.6.2",
+                "jalali-moment": "^3.3.11",
+                "moment": "^2.29.4",
+                "next": "13.1.2",
+                "next-compose-plugins": "^2.2.1",
+                "next-plugin-antd-less": "^1.8.0",
+                "next-redux-wrapper": "^8.1.0",
+                "next-seo": "^6.0.0",
+                "next-with-less": "^3.0.1",
+                "react": "18.2.0",
+                "react-dom": "18.2.0",
+                "react-qr-reader": "^3.0.0-beta-1",
+                "react-quill": "^2.0.0",
+                "react-redux": "^8.0.5",
+                "react-reveal": "^1.2.2",
+                "react-svg-to-image": "^3.0.0",
+                "react-to-print": "^2.14.13",
+                "react-transition-group": "^4.4.5",
+                "redux": "^4.2.0",
+                "redux-thunk": "^2.4.2",
+                "sharp": "^0.31.3",
+                "slugify-persian": "^1.0.1",
+                "swiper": "^9.1.0"
+              },
+              "devDependencies": {
+                "@types/node": "18.11.8",
+                "@types/react": "18.0.24",
+                "@types/react-dom": "18.0.8",
+                "@types/react-qr-reader": "^2.1.4",
+                "@types/react-transition-group": "^4.4.5",
+                "eslint": "8.26.0",
+                "eslint-config-next": "13.0.0",
+                "typescript": "4.8.4"
+              }
+            }
+            
+          `,
+            async (err) => {
+              if (err) {
+                console.error(err);
+              }
+              await exec(
+                `cd ${dist} && pm2 start yarn --name "${item.slug}" bash -- start`,
+              );
+
+              await exec(
+                `sudo ln -s /etc/nginx/sites-available/${item.domain}.conf /etc/nginx/sites-enabled/`,
+              );
+            },
           );
 
           await exec(`sudo service nginx reload`);

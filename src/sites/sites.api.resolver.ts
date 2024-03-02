0a362cbd88b93df257e79bd6cf34d4a69cb43d46
Seena <sinahamzezade@gmail.com>
2024-03-02 11:56:46 +0330

    Add get sites

--- src/sites/sites.api.resolver.ts
+++ src/sites/sites.api.resolver.ts
@@ -1,6 +1,8 @@
 import { Resolver, Query, Args, Int } from '@nestjs/graphql';
 import { SitesService } from './sites.service';
 import { Site } from './entities/site.entity';
+import { SitePaginate } from './entities/paginate';
+import { GetSitesArgs } from './dto/get-items';
 
 @Resolver(() => Site)
 export class SitesResolverApi {
@@ -9,5 +11,10 @@ export class SitesResolverApi {
   @Query(() => Site, { name: 'getSite' })
   findOne(@Args('id', { type: () => Int }) id: number) {
     return this.sitesService.findOne(id);
+  }
+
+  @Query(() => SitePaginate, { name: 'getSites' })
+  findAll(@Args('input') getSitesArgs: GetSitesArgs) {
+    return this.sitesService.findAll(getSitesArgs);
   }
 }
